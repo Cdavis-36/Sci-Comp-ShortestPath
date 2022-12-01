40a287167b7e4d20de11ff8be4c7c604a908348c
Brittany Larsen <blarsen2@student.fitchburgstate.edu>
2022-12-01 14:46:28 -0500

    It's a jl

---
+++ Part 3.jl
@@ -0,0 +1,6 @@
+Write a test suite for your module with the following:
+    (a) Test that creating a vertex works.
+    (b) Test that creating an edge works.
+    (c) Test that creating a basic weighted graph works.
+    (d) Test that addsa singlevertex, a vector of vertices or any number of vertices.
+    (e) Tests that should throw and ArgumentError when addingvertices adds a vertex that already exists.
