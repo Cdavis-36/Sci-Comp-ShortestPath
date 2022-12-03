a9ad6ea236a9ab26876995519eaa0261c8353e8b
Brittany Larsen <blarsen2@student.fitchburgstate.edu>
2022-12-02 20:21:38 -0500

    Did the things

--- Part 3.jl
+++ Part 3.jl
@@ -6,9 +6,11 @@ using Test
 v1 = Vertex(:a)
 v2 = Vertex(:b)
 v3 = Vertex(:c)
+v4 = Vertex(:d)
+v5 = Vertex(:e)
    
     @testset "Vertex" begin
-      @test isa(v1,Vertex)
+      @test isa(v1, Vertex)
       @test isa(v2, Vertex)
       @test isa(v3, Vertex)
     end
@@ -22,10 +24,38 @@ e3=Edge(:b, :c, 5)
        @test isa(e1, Edge)
        @test isa(e2, Edge)
     end
-                    
             
     # (c) Test that creating a basic weighted graph works.
+g=WeightedGraph()
+
+    @testset "Weighted Graph" begin
+        @test isa(g, WeightedGraph)
+    end
 
     # (d) Test that adds a single vertex, a vector of vertices or any number of vertices.
+    @testset "Add Vertex" begin
+      addVertex!(g,v1)
+      @test isa(g, WeightedGraph)
+      @test v1 in g.vertices
+    end
 
-    # (e) Tests that should throw and ArgumentError when addingvertices adds a vertex that already exists.
+   @testset "Add Vector of Vertices" begin
+    addVertices!(g, [v2,v3])
+        @test isa(g, WeightedGraph)
+        @test v2 in g.vertices
+        @test v3 in g.vertices
+   end
+  
+    @testset "Add Vertices" begin
+    addVertices!(g, v4,v5)
+        @test isa(g, WeightedGraph)
+        @test v4 in g.vertices
+        @test v5 in g.vertices
+   end
+
+    # (e) Tests that should throw an ArgumentError when adding vertices adds a vertex that already exists.
+   @testset "This vertex already exists" begin  
+        @test_throws ArgumentError addVertex!(g,v1)
+        @test_throws ArgumentError addVertices!(g, [v2,v3])
+        @test_throws ArgumentError addVertices!(g, v4,v5)
+    end
